58cdc142a9443f01f80274042a5ae8b41496cdaa
Jyotirani Behera <100834992+jyoti-03@users.noreply.github.com>
2024-10-16 17:17:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import mean_var_std
 from unittest import main
 
-print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+# Example usage of the calculate function
+print(mean_var_std.calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]))
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
